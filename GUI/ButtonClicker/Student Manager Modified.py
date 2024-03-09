6c41773af5321c3e37afd479e70e852f5a33ae36
ChaniruYaneth <ihcyaneth12@gmail.com>
2024-03-10 10:27:10 +1300

    Deleted Comments

--- GUI/ButtonClicker/Student Manager Modified.py
+++ GUI/ButtonClicker/Student Manager Modified.py
@@ -5,7 +5,7 @@ class Student:
    
     def __init__(self, name):
         self.name = name
-        self.grade = ""  # Initialize grade attribute
+        self.grade = "" 
    
     def get_grade(self):
         return self.grade
@@ -15,10 +15,10 @@ class Student:
        
        
 def show_grade():
-    global grade_label  # Define grade_label as a global variable
+    global grade_label 
     selected_index = students_listbox.curselection()[0]
     selected_student = csc_2[selected_index]
-    grade_label.config(text=selected_student.get_grade())  # Retrieve grade for the selected student
+    grade_label.config(text=selected_student.get_grade())
        
        
 csc_2 = []
@@ -46,11 +46,11 @@ students_listbox.insert(0, "Rehaan")
 students_listbox.insert(0, "Aaron")
 
 
-grade_label = Label(window)  # Specify parent widget for grade_label
+grade_label = Label(window) 
 grade_label.pack()
 
 
-show_grade_btn = Button(window, text="Show Grade", command=show_grade)  # Specify parent widget for show_grade_btn
+show_grade_btn = Button(window, text="Show Grade", command=show_grade)
 show_grade_btn.pack()
 
 window.mainloop()
