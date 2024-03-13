31922c4fe4e52db400a737aa6be357eecdac403b
ChaniruYaneth <ihcyaneth12@gmail.com>
2024-03-13 18:01:11 +1300

    Corrected the invalid syntax in the code.

--- GUI/ButtonClicker/NZ road rules game.py
+++ GUI/ButtonClicker/NZ road rules game.py
@@ -1,33 +1,37 @@
 from tkinter import *
-names = []
+names_list = []
 
 class QuizStarter:
     def __init__(self, parent):
+        
         background_color="Oldlace"
 
         self.quiz_frame=Frame(parent, bg = background_color, padx=100, pady=100)
         self.quiz_frame.grid()
 
-        self.heading_label=Label(self.quiz_frame, text="NZ Road Rules", font=("Tw Cen MT", "18", "bold"), bg=background_color)
-        self.heading_label.grid(row=0, padx=20)
+        self.heading_label = Label(self.quiz_frame, text="NZ Road Rules", font=("Tw Cen MT", "18", "bold"), bg= background_color)
+        self.heading_label.grid(row=0)
 
-        self.var1=IntVar()
+        self.var1 = IntVar()
         self.user_label=Label(self.quiz_frame, text="Please enter your username below: ", font=("Tw Cen MT", "16"), bg=background_color)
-        self.user_label.grid(row=1, padx=20, pady=20)
+        self.user_label.grid(row=1, pady=20)
 
         self.entry_box=Entry(self.quiz_frame)
-        self.entry_box.grid(row=0, padx=20, pady=20)
+        self.entry_box.grid(row=2, pady=20)
 
         self.continue_button = Button(self.quiz_frame, text="Continue", font=("Helvetica", "13", "bold"), bg="orange", command=self.name_collection)
-        self.continue_button.grid(row=0, padx=20, pady=20)
+        self.continue_button.grid(row=3, pady=20)
 
 def name_collection(self):
     name=self.entry_box.get()
-    names.append(name)
+    names_list.append(name)
+    print(names_list)
     self.quiz_frame.destroy()
+
 
 
 if __name__ == "__main__":
     root = Tk()
     root.title("NZ Road Rules Quiz")
+    quizStarter_object = QuizStarter(root)
     root.mainloop()
